6c4c3e5929bc4e77039d456ea6983b96de9a5bff
MATT FILBERT <mcfilbert@email.wm.edu>
2017-07-24 09:33:34 -0400

    finished part 3

--- oop.rb
+++ oop.rb
@@ -4,15 +4,22 @@
 # - A `greet` instance method
 # - The ability to "get" and "set" `name`
 # Type your solution directly below this line:
-
+class Animal
+  attr_accessor :name
+  def initialize(name)
+  @name = name
+  end
+  def greet
+    puts "Hello, my name is #{@name}"
+  end
+end
 
 
 
 # Question 7
 # Create a new `Animal` instance with the name "Pumba".
 # Type your solution directly below this line:
-
-
+pumba = Animal.new("Pumba")
 
 
 # Question 8
@@ -22,11 +29,21 @@
 # - A `king` (Boolean) attribute
 # - Only set the `king` attribute to `true` if the instance's `name` is "Simba"
 # Type your solution directly below this line:
-
-
-
+class Lion < Animal
+  attr_accessor :name
+  attr_reader :king
+  def initialize(name)
+    super(name)
+    @king = false
+    if @name == "Simba"
+      @king = true
+    end
+  end
+end
 
 
 # Question 9
 # Create a new instance of `Lion` with the name "Simba".
 # Type your solution directly below this line:
+simba = Lion.new("Simba")
+puts simba.king
